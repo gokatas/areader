10b85978c71817e30be8af29b6f9cd07da848bad
Jozef Reisinger <jozef.reisinger@gmail.com>
2024-06-10 09:01:37 +0200

    use test from go tour code

--- areader_test.go
+++ areader_test.go
@@ -1,18 +1,29 @@
 package areader_test
 
 import (
-	"bytes"
 	"testing"
 
 	"github.com/gokatas/areader"
 )
 
-func TestAEmitsThreeAs(t *testing.T) {
-	want := []byte("AAA")
-	got := make([]byte, 3)
+// Adapted from https://go.googlesource.com/tour/+/master/reader/validate.go
+func TestRead(t *testing.T) {
 	var a areader.A
-	a.Read(got)
-	if !bytes.Equal(want, got) {
-		t.Errorf("want %v, got %v", want, got)
+	b := make([]byte, 1024, 2048)
+	i, o := 0, 0
+	for ; i < 1<<20 && o < 1<<20; i++ { // test 1mb
+		n, err := a.Read(b)
+		for i, v := range b[:n] {
+			if v != 'A' {
+				t.Errorf("got byte %x at offset %v, want 'A'\n", v, o+i)
+			}
+		}
+		o += n
+		if err != nil {
+			t.Errorf("read error: %v\n", err)
+		}
+	}
+	if o == 0 {
+		t.Errorf("read zero bytes after %d Read calls\n", i)
 	}
 }
